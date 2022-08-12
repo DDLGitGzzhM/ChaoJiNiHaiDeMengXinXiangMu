30318cd1fe7a427058932abd2511e555b9551475
Gzm <979218087@qq.com>
2022-08-12 17:39:11 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
@@ -51,16 +52,14 @@ header {
 </template>
 
 <script>
+=======
+<script setup>
+>>>>>>> e4fe8eb (初始化)
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
+<<<<<<< HEAD
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -70,5 +69,47 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 >>>>>>> f332228 (init)
+=======
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+>>>>>>> e4fe8eb (初始化)
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './assets/main.css'
 
 =======
 >>>>>>> f332228 (init)
+=======
+import './assets/main.css'
+
+>>>>>>> e4fe8eb (初始化)
 createApp(App).mount('#app')
